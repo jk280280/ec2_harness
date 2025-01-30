f492c613663dcba18fee6d73be43c4298d51abc2
jk280280 <jk280280@gmail.com>
2025-01-30 15:35:07 +0530

    second commit

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "my_first_ec2_instance" {
   instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
   
   tags = {
-    Name = "my_ec3_instance"
+    Name = "my_ecs_instance"
   }
 }
