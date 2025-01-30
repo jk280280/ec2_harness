bef264be35c664c960f20d6c705dc83698bedbe9
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-01-30 12:32:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_first_ec2_instance" {
+  ami = "ami-0f214d1b3d031dc53" 
+  instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
+  
+  tags = {
+    Name = "my_first_ec2_instance"
+  }
+}
