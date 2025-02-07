cd7c03747235e7b4df859d49e4b5d015fe104416
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-02-07 12:35:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_first_ec2_instance" {
   ami = "ami-0f214d1b3d031dc53" 
-  instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
+  instance_type = "t2.micro"
   
   tags = {
     Name = "my_ecs_instance"
